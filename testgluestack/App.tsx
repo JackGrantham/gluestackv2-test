0b84de5176838bc8d44ff49c12e04eb489e0b115
viraj-10 <virajjoshi132@gmail.com>
2024-11-05 20:24:33 +0530

    fix: expo issue

--- testgluestack/App.tsx
+++ testgluestack/App.tsx
@@ -1,28 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import "@/global.css";
+import { StatusBar } from "expo-status-bar";
+import "./global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, ButtonText } from './components/ui/button';
+import { StyleSheet, Text, View } from "react-native";
+import { Button, ButtonText } from "./components/ui/button";
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light"><View style={styles.container}>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
-        
-        <Button size="md" variant="solid" action="primary" >
+        <Button size="md" variant="solid" action="primary">
           <ButtonText>Hello World!</ButtonText>
         </Button>
-      
         <StatusBar style="auto" />
-      </View></GluestackUIProvider>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
